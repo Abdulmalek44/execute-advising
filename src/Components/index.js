af99d259067fe4bcfc8277416777ee73f588200a
Abdulmalek Al-Aktaa <132466477+Abdulmalek44@users.noreply.github.com>
2023-05-27 13:25:50 +0300

    Delete src directory

--- src/Components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from './About/About';
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Hero } from './Hero/Hero';
-export { default as Testimonials } from './Testimonials/Testimonials';
-export { default as Demo } from './Demo/Demo';
-export { default as Footer } from './Footer/Footer';
